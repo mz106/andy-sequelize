860062b2d7de05e089824ff3ac3ad2bb10e732b2
Michael Z <michaelzwierzanski@gmail.com>
2023-04-20 15:48:49 +0100

    health route

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/health", (req, res) => {
+  res.send("App is healthy");
+});
+
 app.listen(5001, () => {
   console.log("App is listening");
 });
